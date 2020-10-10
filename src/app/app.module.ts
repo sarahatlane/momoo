7d594620efd399339ef0b593c758535c88f571b6
sam <sam@thedigitalacademy.co.za>
2020-10-11 01:16:35 +0200

    generated component for someone who is rendering a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FilterComponent } from './components/filter/filter.component';
 import { JobComponent } from './components/job/job.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { ServiceComponent } from './components/service/service.component';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { LoginComponent } from './components/login/login.component';
     JobComponent,
     RegisterComponent,
     LoginComponent,
+    ServiceComponent,
 
   ],
   imports: [
